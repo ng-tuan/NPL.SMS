9c5038cbddf3142d0e76bf7d6cae92bb05c486d8
ng-tuan <ntuan.work@gmail.com>
2021-11-22 20:20:50 +0700

    update AddCustomer method and ComputeOrderTotal method

--- NPL.SMS/R2S.Training.Dao/CustomerDAO.cs
+++ NPL.SMS/R2S.Training.Dao/CustomerDAO.cs
@@ -1,13 +1,17 @@
-﻿using NPL.SMS.R2S.Training.Entities;
-using NPL.SMS.R2S.Training.Entities.R2S.Training.Dao;
-using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Text;
+using System;
+using NPL.SMS.R2S.Training.Entities.R2S.Training.Dao;
+using NPL.SMS.R2S.Training.Entities;
+using NPL.SMS.R2S.Training.Main;
 
 namespace NPL.SMS.R2S.Training.Dao
-{
+{    
     class CustomerDAO : ICustomerDAO
     {
+        private const string ADD_CUSTOMER = "sp_add_customer";
         public List<Customer> GetAllCustoners()
         {
             throw new NotImplementedException();
@@ -16,6 +20,39 @@ namespace NPL.SMS.R2S.Training.Dao
         public List<Order> GetAllOrdersByCustomerID(int customerId)
         {
             throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Add new a customer using Store Procedure
+        /// </summary>
+        /// <param name="customer">Customer information</param>
+        /// <returns>Logic status</returns>
+        public bool AddCustomer(Customer customer)
+        {
+            // Create a connection
+            using SqlConnection conn = Connect.GetSqlConnection();
+
+            // Open a connection
+            conn.Open();
+
+            // Create parameter
+            SqlParameter param = new SqlParameter
+            {
+                ParameterName = "@customer_name",
+                Value = customer.CustomerName
+            };
+
+            // Create a sql command
+            using SqlCommand cmd = Connect.GetSqlCommand(ADD_CUSTOMER, conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            // Add parameter to sql command
+            cmd.Parameters.Add(param);
+
+            // Execute sql delete command and return logic status
+            if (cmd.ExecuteNonQuery() > 0)
+                return true;
+            else return false;           
         }
     }
 }
---
+++ NPL.SMS/R2S.Training.Dao/OrderDao.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+using NPL.SMS.R2S.Training.Main;
+
+namespace NPL.SMS.R2S.Training.Dao
+{
+    class OrderDao : IOrderDao
+    {        
+        private const string COMPUTE_ORDER_TOTAL = "select dbo.fn_compute_order_total(@order_id)";
+
+        /// <summary>
+        /// Compute order total using Function
+        /// </summary>        
+        /// <param name="orderId">Order information</param>
+        /// <returns></returns>
+        public double ComputeOrderTotal(int orderId)
+        {
+            // Create a connection
+            using SqlConnection conn = Connect.GetSqlConnection();
+
+            // Create a sql command
+            using SqlCommand cmd = Connect.GetSqlCommand(COMPUTE_ORDER_TOTAL, conn);
+
+            // Create parameter
+            SqlParameter param = new SqlParameter
+            {
+                ParameterName = "@order_id",
+                Value = orderId
+            };
+
+            // Add parameter to sql command
+            cmd.Parameters.Add(param);
+
+            try
+            {
+                // Open a connection
+                conn.Open();
+
+                // Execute sql compute order total statement 
+                return (double)cmd.ExecuteScalar();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return 0;
+        }
+    }
+}
