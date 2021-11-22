8b30a2d4a92d3b0e9e052c58650e332b0827ca95
Đăng Nguyễn Duy <dang01635441895@gmail.com>
2021-11-22 20:57:52 +0700

    Update ILineItemDAO Interface

---
+++ NPL.SMS/R2S.Training.Dao/ILineItemDAO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NPL.SMS.R2S.Training.Entities.R2S.Training.Dao
+{
+    interface ILineItemDAO
+    {
+        List<LineItem> GetAllItemByOrderId(int orderId);//Yêu cầu 3
+        bool AddLineItem(LineItem item);    //yêu cầu 9
+    }
+}
