b80fbdbb9db5975071e007b5606e2c2d9b0a57c1
ng-tuan <ntuan.work@gmail.com>
2021-11-22 20:20:50 +0700

    update project

--- NPL.SMS/R2S.Training.Entities/Order.cs
+++ NPL.SMS/R2S.Training.Entities/Order.cs
@@ -6,5 +6,28 @@ namespace NPL.SMS.R2S.Training.Entities
 {
     class Order
     {
+        private int orderId, customerId, employeeId;
+        private double total;
+        private DateTime orderDate;
+        public Order()
+        {
+        }
+        public Order(int orderId, DateTime orderDate, int customerId, int employeeId, double total)
+        {
+            this.orderId = orderId;
+            this.orderDate = orderDate;
+            this.customerId = customerId;
+            this.employeeId = employeeId;
+            this.total = total;
+        }
+        public int OrderId { get => orderId; set => orderId = value; }
+        public DateTime OrderDate { get => orderDate; set => orderDate = value; }
+        public int CustomerId { get => customerId; set => customerId = value; }
+        public int EmployeeId { get => employeeId; set => employeeId = value; }
+        public double Total { get => total; set => total = value; }
+        public override string ToString()
+        {
+            return string.Format("Order ID: {0}, Order time: {1}, Customer ID: {2}, Employee ID: {3}, Total: {4}", OrderId, OrderDate, CustomerId, EmployeeId, Total);
+        }
     }
 }
