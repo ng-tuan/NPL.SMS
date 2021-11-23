1e180726af4ca98fda679cb8ddd74689b027e8f9
Đăng Nguyễn Duy <dang01635441895@gmail.com>
2021-11-23 16:15:00 +0700

    update case 3 - 9

--- NPL.SMS/R2S.Training.Main/SaleManagement.cs
+++ NPL.SMS/R2S.Training.Main/SaleManagement.cs
@@ -15,13 +15,13 @@ namespace NPL.SMS.R2S.Training.Main
             Console.WriteLine(
                 "1. Get list customers in the Orders table" +
                 "\n2. Get all orders for a given customerId" +
-                "\n3. Get all orders for a given customerId" +
+                "\n3. Get all lineitem for a given orderId" +
                 "\n4. Get all orders for a given customerId" +
                 "\n5. Get all orders for a given customerId" +
                 "\n6. Get all orders for a given customerId" +
                 "\n7. Get all orders for a given customerId" +
                 "\n8. Get all orders for a given customerId" +
-                "\n9. Get all orders for a given customerId" +
+                "\n9. Add line item" +
                 "\n10. Get all orders for a given customerId" +
                 "\n11. Exit"
                 );
@@ -35,6 +35,8 @@ namespace NPL.SMS.R2S.Training.Main
             string option;
             const string LIST_CUSTOMERS = "1";
             const string LIST_ORDERS = "2";
+            const string LIST_LINEITEM = "3";
+            const string ADD_ITEM = "9";
             const string EXIT = "11";
 
             CustomerDAO CD = new CustomerDAO();
@@ -84,7 +86,39 @@ namespace NPL.SMS.R2S.Training.Main
                             }
                         }
                         break;
+                    case LIST_LINEITEM:
+                        Console.Write("Enter order Id: ");
+                        int orderId = int.Parse(Console.ReadLine());
 
+                        try
+                        {
+                            if (LineItemDAO.CheckOrderId(orderId))
+                            {
+                                Console.WriteLine("----------LIST LINE ITEM OF ORDER ID-----------");
+                                foreach (LineItem item in LD.GetAllItemByOrderId(orderId))
+                                {
+                                    Console.WriteLine(item);
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("This order id not exits");
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+                        break;
+                    case  ADD_ITEM: 
+                        Console.WriteLine("--------ADD LINE ITEM--------");
+                        LineItem lineItem = new LineItem();
+                        lineItem.AddInfor();
+                        if (LD.AddLineItem(lineItem))
+                            Console.WriteLine("Successfully!!");
+                        else
+                            Console.WriteLine("Failed");
+                        break;
                 }
             }
             while (option != EXIT);  
