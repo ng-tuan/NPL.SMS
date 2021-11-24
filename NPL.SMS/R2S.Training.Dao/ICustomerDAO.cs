95c85f88951a3986c8bdcf618e3bc8d997dd12ee
Thinh-Kys <94960441+Thinh-Kys@users.noreply.github.com>
2021-11-24 20:20:37 +0700

    Update ICustomerDAO.cs

--- NPL.SMS/R2S.Training.Dao/ICustomerDAO.cs
+++ NPL.SMS/R2S.Training.Dao/ICustomerDAO.cs
@@ -9,5 +9,7 @@ namespace NPL.SMS.R2S.Training.Entities.R2S.Training.Dao
         List<Customer> GetAllCustomers(); // functional 1  
         List<Order> GetAllOrdersByCustomerID(int customerId); // functional 2
         bool AddCustomer(Customer customer); //functional 5
+        bool UpdateCustomer(Customer customer); //func 6
+        bool DeleteCustomer(int customerId); //func 7
     }
 }
