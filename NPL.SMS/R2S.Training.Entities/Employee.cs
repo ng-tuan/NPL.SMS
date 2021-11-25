13078ef9352701cdbb9ae5addbb7b11b9bc3f8b2
Thinh-Kys <94960441+Thinh-Kys@users.noreply.github.com>
2021-11-25 15:09:38 +0700

    Add files via upload

---
+++ NPL.SMS/R2S.Training.Entities/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NPL.SMS.R2S.Training.Entities
+{
+    class Employee
+    {
+        private int employeeId;
+        private string employeeName;
+        private double salary;
+        private int spvrId;
+
+        public Employee()
+        {
+
+        }
+        public Employee(int employeeId, string employeeName, double salary, int spvrId)
+        {
+
+        }
+
+        public int EmployeeId { get => employeeId; set => employeeId = value; }
+        public string EmployeeName { get => employeeName; set => employeeName = value; }
+        public double Salary { get => salary; set => salary = value; }
+        public int SpvrId { get => spvrId; set => salary = value; }
+
+    }
+}
