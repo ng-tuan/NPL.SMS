299d3f457ff06d1d4ff1f4f814266f2355840fe8
ng-tuan <ntuan.work@gmail.com>
2021-11-26 19:28:22 +0700

    update project

--- NPL.SMS/R2S.Training.Entities/Product.cs
+++ NPL.SMS/R2S.Training.Entities/Product.cs
@@ -11,16 +11,54 @@ namespace NPL.SMS.R2S.Training.Entities
         private double productPrice;
 
         public Product()
-        {}
+        { }
         public Product(int productId, string productName, double productPrice)
         {
-            this.ProductId  = productId;
+            this.ProductId = productId;
             this.ProductName = productName;
             this.ProductPrice = productPrice;
         }
-        public double ProductPrice { get => productPrice; set => productPrice = value; }
-        public int ProductId { get => productId;  set => productId = value;  }
-        public string ProductName {  get => productName; set => productName = value; }
+        public double ProductPrice
+        {
+            get { return productPrice; }
+            set { productPrice = value; }
+        }
+        public int ProductId
+        {
+            get { return productId; }
+            set
+            {
+                if (value > 0)
+                {
+                    productId = value;
+                }
+                else
+                {
+                    do
+                    {
+                        Console.WriteLine("ProductID must > 0, enter again: ");
+                        productId = int.Parse(Console.ReadLine());
+                    } while (productId <= 0);
+                }
+            }
+        }
+        public string ProductName
+        { get => productName; set => productName = value; }
+        public void AddInfor()
+        {
+            Console.Write("Input Order ID: ");
+            ProductId = int.Parse(Console.ReadLine());
+
+            Console.Write("Input Product Name: ");
+            ProductName = Console.ReadLine();
+
+            Console.Write("Input Product Price: ");
+            ProductPrice = double.Parse(Console.ReadLine());
+        }
+        public override string ToString()
+        {
+            return String.Format("Product ID: {0}, Product Name: {1}, Product Price: {2}", productId, productName, ProductPrice);
+        }
     }
 }
 
