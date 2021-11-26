48ad079bedc169440f22498608b3aab6a608b2ca
Nguyễn Đức Tây <57629621+elcoco33@users.noreply.github.com>
2021-11-27 02:15:56 +0700

    Update IOrderDao.cs

--- NPL.SMS/R2S.Training.Dao/IOrderDao.cs
+++ NPL.SMS/R2S.Training.Dao/IOrderDao.cs
@@ -7,7 +7,7 @@ namespace NPL.SMS.R2S.Training.Entities.R2S.Training.Dao
     interface IOrderDAO
     {
         double ComputeOrderTotal(int orderId); // functional 4                                      
-        bool AddOrder(LineItem item);    //function 8
+        bool AddOrder(Order order);    //function 8
         bool UpdateOrderTotal(int orderId); //function 10
     }
 }
